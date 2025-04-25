40869e492cffe81ed70f76a5a7d3c24ca4f7c147
urkiddennn <154726035+urkiddennn@users.noreply.github.com>
2025-04-25 17:56:45 +0800

    dsadas

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 // Apply CORS middleware
 app.use(
     cors({
-        origin: ['https://cg3-solar-products.vercel.app/'],
+        origin: ['https://cg3-solar-products.vercel.app'],
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
         allowedHeaders: ['Content-Type', 'Authorization'],
     })
